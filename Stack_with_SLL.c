949a2a124d66205047dd332c12db9cb5ee7c0d8a
Thoran37 <132071612+Thoran37@users.noreply.github.com>
2023-05-04 09:52:08 +0530

    Update Stack_with_SLL.c

--- Stack_with_SLL.c
+++ Stack_with_SLL.c
@@ -4,50 +4,27 @@ struct node
 {
   int x;
   struct node*link;
-}*first=NULL,*temp,*nn,*last=NULL;
-void create()
-{ 
-	printf("Enter -1 to stop\n");
-  while(1)
-    {
-      nn=(struct node*)malloc(sizeof(struct node));
-      printf("Enter data  ");
-      scanf("%d",&nn->x);
-      if(nn->x==-1)
-        break;
-      if(first==NULL)
-        {
-        	first=nn;
-          last=nn;
-          last->link=NULL;
-        }
-      else
-        {
-          nn->link=last;
-          last=nn;
-        }
-    }
-}
-int count(struct node*f)
+}*temp,*nn,*top=NULL;
+int count()
 {
   int cnt=0;
-  for(temp=last;temp!=NULL;temp=temp->link)
+  for(temp=top;temp!=NULL;temp=temp->link)
     cnt++;
   return cnt;  
 }
-void display(struct node*f)
+void display()
 {
-  for(temp=last;temp!=NULL;temp=temp->link)
+  for(temp=top;temp!=NULL;temp=temp->link)
     printf("%d ",temp->x);  
 }
-void pop(struct node*f)
+void pop()
 {
   if(f==NULL)
     printf("LQ is empty\n");
   else 
     {
-      temp=last;
-      last=last->link;
+      temp=top;
+      top=top->link;
       temp->link=NULL;
       printf("Deleted element is %d\n",temp->x);
       free(temp);
@@ -58,33 +35,32 @@ void push(int k)
   nn=(struct node*)malloc(sizeof(struct node));
   if(first==NULL)
     {
-      first=nn;
-      last=nn;
-      last->link=NULL;
+      top=nn;
+		  nn->x=k;
+      top->link=NULL;
     }
   else
     {  
-      nn->link=last;
+      nn->link=top;
       nn->x=k;
-      last=nn;
+      top=nn;
     }
 }
 int main()
 {
   int k,choice;
-  printf("1.create 2.delete 3.insert 4.display 5.count  ");
+  printf("1.insert 2.delete 3.display 4.count  ");
   do
     {
       scanf("%d",&choice);
       switch(choice)
         {
-          case 1: create();break;
-          case 2: pop(first);break;
-          case 3: printf("Enter element to insert  ");
+          case 2: pop();break;
+          case 1: printf("Enter element to insert  ");
                   scanf("%d",&k);
                   push(k);break;
-          case 4: display(first);break;        
-          case 5: printf("No.of elements = %d\n",count(first));break;
+          case 3: display();break;        
+          case 4: printf("No.of elements = %d\n",count());break;
           default: printf("Wrong choice\n");        
         }
       printf("Continue 1/0  ");

445602630f8183efe621fd25eb93b0d5cb66a246
Thoran37 <132071612+Thoran37@users.noreply.github.com>
2023-05-16 15:33:25 +0530

    Update tree_construction.c

--- tree_construction.c
+++ tree_construction.c
@@ -1,85 +1,135 @@
-#include<stdio.h>
-#include<stdlib.h>
-int top=-1;
+#include <stdio.h>
+#include <stdlib.h>
+#define size 300
 struct node
 {
-  struct node*llink;
-  int x;
-  struct node* rlink;
-}*root=NULL,*temp,*nn,*stack;
+	struct node *l,*r;
+	char c;
+}*root=NULL,*nn,*ptr;
+
+struct node *stack[size];
+
+int top=-1;
+
 void well()
 {
-  nn=(struct node*)malloc(sizeof(struct node));
-  printf("Enter data  ");
-  scanf("%d",&nn->x);
+	nn=(struct node*)malloc(sizeof(struct node));
+	printf("Enter data  ");
+	fflush(stdin);
+	scanf("%c",&nn->c);
 }
 void push(struct node *k)
 {
-	top++;
-  *(stack+top)=*k;
+  stack[++top]=k;
 }
 struct node* pop()
 {
-	struct node *p=(stack+top);
-  top--;
-  return p;
+	return stack[top--];
 }
-struct node* create_tree(struct node*f)
+struct node *createbt(struct node *t)
 {
-  char ch,s;
-  do
-    {
-      if(root==NULL)
-        {
-        	well();
-          root=nn;
-          temp=nn;
-          temp->llink=NULL;
-          temp->rlink=NULL;
-        }
-      else
-        {
-          printf("Enter side  ");
-          fflush(stdin);
-          ch=getchar();
-          well();
-          if(ch=='l')
-            {
-              temp->llink=nn;
-              temp->rlink=NULL;
-            }
-          else if(ch=='r')
-            {
-              temp->rlink=nn;
-              temp->llink=NULL;
-            }
-          else
-            printf("Wrong side\n"); 
-        }  
-      printf("Continue main y/n  ");
-			fflush(stdin); 
-      s=getchar();
-    }while(s=='y');
+	int k;
+	push(NULL);
+	well();
+	t=nn;
+	ptr=nn;
+	while(ptr!=NULL)
+	  {
+	  	printf("Right child for %c 1/0  ",ptr->c);
+	  	scanf("%d",&k);
+	  	if(k==1)
+	  	  {
+	  	    well();
+	  	  	ptr->r=nn;
+	  	  	push(ptr->r);
+				}
+			else
+			  ptr->r=NULL;
+			printf("Left child for %c 1/0  ",ptr->c);
+			scanf("%d",&k);
+			if(k==1)
+			  {
+  			  well();
+					ptr->l=nn;  	
+					push(ptr->l);
+				}		
+			else
+			  ptr->l=NULL;
+			ptr=pop();		
+		}
+	return t;	
 }
-void display_inorder(struct node*f)
+void in_non(struct node *f)
 {
-  struct node *ptr;
   ptr=root;
   push(NULL);
   while(ptr!=NULL)
     {
       push(ptr);
-      ptr=ptr->llink;
-    }   
-  if(ptr==NULL)
-  {
-    ptr=pop();
-    printf("%d ",ptr->x);
-    ptr=ptr->rlink;
-  }   
+      ptr=ptr->l;
+      if(ptr==NULL)
+			  {
+			    ptr=pop();
+			    printf("%c ",ptr->c);
+			    ptr=ptr->r;
+			  }
+		}
+}
+void pre_non(struct node *t)
+{
+	ptr=root;
+	push(NULL);
+	while(ptr!=NULL)
+	  {
+	  	printf("%c ",ptr->c);
+	  	if(ptr->r!=NULL)
+        push(ptr->r);
+      ptr=ptr->l;
+	  	if(ptr==NULL)
+    	  ptr=pop();
+		}
+}
+void post_non(struct node *t)
+{
+	ptr=root;
+	push(NULL);
+	while(ptr!=NULL)
+    {
+      push(ptr);
+      ptr=ptr->l;
+      if(ptr==NULL)
+		    ptr=pop();
+			if(ptr->r!=NULL)
+			  ptr=ptr->r;
+			else
+			  {
+			    printf("%c ",ptr);  
+			    ptr=pop();
+			  }
+		}
 }
 int main()
 {
-	root=create_tree(root);
-	display_inorder(root);
+	int k;
+	printf("1.create 2.inorder 3.preorder 4.postorder 5.in_non 6.pre_non 7.post_non 8.height 9.leaf 10.no_leaf  ");
+	do
+	  {
+	  	scanf("%d",&k);
+	  	switch(k)
+	  	  {
+	  	  	case 1: root=createbt(root);break;
+	  	  	//case 2: inorder(root);break;
+	  	  	//case 3: preorder(root);break;
+	  	  	//case 4: postorder(root);break;
+	  	  	case 5: in_non(root);break;
+	  	  	case 6: pre_non(root);break;
+	  	  	case 7: post_non(root);break;
+	  	  	//case 8: printf("Height = %d\n",height());break;
+	  	  	//case 9: printf("No.of leaf node = %d\n",countln());break;
+	  	  	//case 10: printf("No.of non leaf node = %d\n",countnln());break;*/
+	  	  	default: printf("Wrong choice\n");
+				}
+			printf("Continue main 1/0  ");
+			scanf("%d",&k);
+		}while(k==1);
 }

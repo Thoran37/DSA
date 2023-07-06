d303633d363c5a6cb5814d38bb6c1c92e0dd454d
Thoran37 <132071612+Thoran37@users.noreply.github.com>
2023-07-06 11:30:16 +0530

    Update quicksort.c

--- quicksort.c
+++ quicksort.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <time.h>
 int a[10];
 void swap(int *a,int *b)
 {
@@ -34,12 +35,16 @@ void quicksort(int lb,int ub)
 int main()
 {
 	int n;
+	clock_t t;
 	printf("ENter n  ");
 	scanf("%d",&n);
 	printf("Enter %d numbers  ",n);
 	for(int i=0;i<n;i++)
 		scanf("%d",&a[i]);
+	t=clock();	
 	quicksort(0,n-1);	
+	t=clock()-t;
 	for(int i=0;i<n;i++)
 		printf("%d ",a[i]);
+	printf("\nTime taken for recursive quick sort is %f",(float)t/CLOCKS_PER_SEC);	
 }

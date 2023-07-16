a7715305213fb3899634f91232d18fe1fcb88080
Thoran37 <132071612+Thoran37@users.noreply.github.com>
2023-07-16 18:11:54 +0530

    Create Double_hashing.c

---
+++ Double_hashing.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+int D,a[30],b[30],i,r,j,R;
+
+int double_hash(int k, int f)
+{
+  r=(k+j*(R-(f%R)))%D;
+  if(b[r]==0 || j>D)
+    return r;
+  else
+    {
+      j++;
+      double_hash(k,f);
+    }
+}
+
+void hash()
+{
+  int k;
+  for(i=0;i<D;i++)
+    {
+      j=0;
+      k=a[i]%D;
+      if(!b[k])
+        b[k]=a[i];
+      else
+        b[double_hash(k,a[i])]=a[i];  
+    }
+}
+
+int main()
+{
+  int flag;
+  printf("Enter D ");
+  scanf("%d",&D);
+  printf("Enter numbers ");
+  for(i=0;i<D;i++)
+    scanf("%d",&a[i]);
+  for(i=D-1;i>=0;i--)
+    {
+      flag=1;
+      for(int h=2;h<i;h++)
+        if(i%h==0)
+          {
+            flag=0;
+            break;
+          }
+      if(flag)
+        {
+          R=i;    
+          break;
+        }
+    }
+  for(i=0;i<D;i++)
+    b[i]=0;  
+  hash();
+  for(i=0;i<D;i++)
+    printf("%d ",b[i]);
+}

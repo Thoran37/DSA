53daaedc8f01c2af3b311396ae9b19e1632430b0
Thoran37 <132071612+Thoran37@users.noreply.github.com>
2023-07-16 17:09:44 +0530

    Create linearprobing.c

---
+++ linearprobing.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int D,a[30],b[30],i,r,j;
+
+int linear_prob(int k)
+{
+  r=(k+j)%D;
+  if(b[r]==0)
+    return r;
+  else
+    {
+      j++;
+      linear_prob(k);
+    }
+}
+
+void hash()
+{
+  int k;
+  for(i=0;i<D;i++)
+    {
+      j=0;
+      k=a[i]%D;
+      if(!b[k])
+        b[k]=a[i];
+      else
+        b[linear_prob(k)]=a[i];  
+    }
+}
+
+int main()
+{
+  printf("Enter D ");
+  scanf("%d",&D);
+  printf("Enter numbers ");
+  for(i=0;i<D;i++)
+    scanf("%d",&a[i]);
+  hash();
+  for(i=0;i<D;i++)
+    printf("%d ",b[i]);
+}

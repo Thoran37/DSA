a5e03b92d72cb4b4801114787e52354bf76321ef
Thoran37 <132071612+Thoran37@users.noreply.github.com>
2023-07-20 15:10:33 +0530

    Update BFS.c

--- BFS.c
+++ BFS.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int graph[25][25]={0},n,status[25]={0};
+int graph[25][25]={0},status[25]={0};
 int queue[25],f=0,r=-1;
 int visit[25][25]={0},stack[25],top=-1;
 
@@ -19,30 +19,30 @@ void push(int k)
 	stack[++top]=k;
 }
 
-int pop()
+int isempty()
 {
-	return stack[top--];
+	if(f>r)
+	  return 0;
+	else
+	  return 1;  
 }
 
 int main()
 {
-	int index;
-	printf("Enter no.of vertices  ");
-	scanf("%d",&n);
+	int index,d,a,b,h,i,j,p,n;
+	printf("Enter no.of vertices and edges  ");
+	scanf("%d%d",&n,&p);
 //	printf("Enter matrix of input graph\n");
 //	for(int i=0;i<n;i++)
 //	  for(int j=0;j<n;j++)
 //	    scanf("%d",&a[i][j]);
-	printf("Enter -1 to stop\n");
 	printf("Enter source and destination of the edge  ");
-	do
+	while(p--)
 	  {
-			scanf("%d",&a);
-			if(a==-1)
-			  break;
-			scanf("%d",&b);
-			graph[a][b]=1;	  	
-		}while(1);    
+			scanf("%d%d",&a,&b);
+			graph[a][b]=1;	  
+			graph[b][a]=1;	
+		}
 	printf("Enter starting vertex index  ");
 	scanf("%d",&index);
 	enque(index);
@@ -52,11 +52,25 @@ int main()
 			h=deque(); 	
 			status[h]++;
 			push(h);
-			for(int i=0;i<n;i++)
+			for(i=0;i<n;i++)
 			  if(graph[h][i] && status[i]==0)
 			    {
 			      enque(i);
 			      status[i]++;
 			    }
 	  }   
+	for(i=0;i<=top;i++)  
+	  {
+	  	d=0;
+	  	while(graph[stack[i-d]][stack[i+1]]==0)
+	  	  d++;
+  	  visit[stack[i-d]][stack[i+1]]=1;
+  	  visit[stack[i+1]][stack[i-d]]=1;
+		}
+	for(i=0;i<n;i++)
+	  {
+	    for(j=0;j<n;j++)
+		    printf("%d ",visit[i][j]);
+		  printf("\n");		
+		}
 }

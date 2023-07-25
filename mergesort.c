9cdf39452a7d321b9eab7ae62383e715513602eb
Thoran37 <132071612+Thoran37@users.noreply.github.com>
2023-07-25 19:12:31 +0530

    Update mergesort.c

--- mergesort.c
+++ mergesort.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include <time.h>
 int a[10];
 void merge(int lb, int mid, int ub)
 {
   int size1=mid-lb+1,size2=ub-mid,l[size1],r[size2],i,j,k;
-  for(int i=0;i<size1;i++)
+  for(i=0;i<size1;i++)
     l[i]=a[lb+i];
-  for(int i=0;i<size2;i++)
+  for(i=0;i<size2;i++)
     r[i]=a[mid+1+i];
   for(i=0,j=0,k=lb;i<size1 && j<size2;k++)
     {
@@ -46,12 +47,16 @@ void mergesort(int lb, int ub)
 int main()
 {
   int n,i;
+  clock_t t;
   printf("Enter n  ");
   scanf("%d",&n);
   printf("Enter %d values  ",n);
   for(i=1;i<=n;i++)
     scanf("%d",&a[i]);
+  t=clock();   
   mergesort(1,n); 
+  t=clock()-t;
   for(i=1;i<=n;i++)
     printf("%d ",a[i]);
+  printf("\nTime taken by merge sort is %fs",(float)t/CLOCKS_PER_SEC);  
 }

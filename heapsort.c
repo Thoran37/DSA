694d934e7f5cf74105cb38832e413d322581738c
Thoran37 <132071612+Thoran37@users.noreply.github.com>
2023-07-27 11:03:47 +0530

    Update heapsort.c

--- heapsort.c
+++ heapsort.c
@@ -1,41 +1,42 @@
-/* // Heap Sort
+// Heap Sort
 #include <stdio.h>
 #include <time.h>
-int a[100],index=1,n,h=1;
-void swap(int i,int j)
+int a[100],ind=1,n,h=1;
+void reheap();
+void swap(int *a,int *b)
 {
-	int t=a[i];
-  a[i]=a[j];
-  a[j]=t;
+	int t=*a;
+  *a=*b;
+  *b=t;
 }
 void heapsort()
 {
-	if(2*h>n)
-	  {
-	  	h=1;
-	    reheap();
-	  }
-	int max=a[2*h]>a[2*h+1]?2*h:2*h+1;
-	swap(h,max);
-	h=max;
+  while(2*h<n)
+    {  
+      int max=a[2*h]>a[2*h+1]?2*h:2*h+1;
+      swap(&a[h],&a[max]);
+      h=max;
+    }
 }
 void reheap()
 {
 	if(n==1)
 	  return;
-	swap(1,n);
+	swap(&a[1],&a[n]);
 	n--;
 	heapsort();
+  h=1;
+  reheap();
 }
 void insert(int k)
 {
-	a[index++]=k;
-	int i=index;
+	int i=ind;
+	a[ind++]=k;
 	while(i/2)
 	  {
 		  if(a[i]>a[i/2])
 			  {
-				  swap(i,i/2);
+				  swap(&a[i],&a[i/2]);
 				  i/=2;
 			  }
 			else
@@ -58,4 +59,3 @@ int main()
 	for(i=1;i<=k;i++)
 	  printf("%d ",a[i]);	
 }
-*/

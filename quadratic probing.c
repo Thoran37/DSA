90104578834103ae7c9617c6a2a7dccfc523458f
Thoran37 <132071612+Thoran37@users.noreply.github.com>
2023-08-02 19:24:38 +0530

    Update quadratic probing.c

--- quadratic probing.c
+++ quadratic probing.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
-int D,a[10],b[10],i,r,j;
+int D,a[10],b[10]={0},i,r,j,g;
 int quad_prob(int k)
 {
   r=(k+j)%D;
-  if(b[r]==0 || j>D)
+  if(b[r]==0 || g>D)
     return r;
   else
     {
       j++;
+      g++;
       j*=j;
       quad_prob(k);
     }
@@ -17,7 +18,7 @@ void hash()
   int k;
   for(i=0;i<D;i++)
     {
-      j=0;
+      j=0,g=0;
       k=a[i]%D;
       if(!b[k])
         b[k]=a[i];
